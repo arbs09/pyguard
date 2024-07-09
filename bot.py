56ed6ff3c3ecddcc3532fbb3e7249a3425befe2a
arbs09 <info@arbs09.de>
2024-07-09 12:07:13 +0200

    update

--- bot.py
+++ bot.py
@@ -5,12 +5,37 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    print('The Bot is logged in as {0.user}'.format(client))
+
+    print(
+        f'The Bot is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+    
+ 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+ 
+    if message.content.startswith('hi'):
+        await message.channel.send('Hello!')
+    
+     
+    if message.content.startswith('!info'):
+        await message.channel.send('This is a discord bot, coded in python!\nGithub: https://github.com/arbs09/python-discordbot')
 
 client.run(TOKEN)
