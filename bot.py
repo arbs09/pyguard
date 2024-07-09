6dd21dccd5270fee3fecff2f662a5d5452c6fd78
arbs09 <info@arbs09.de>
2024-07-09 12:39:30 +0200

    add echo

--- bot.py
+++ bot.py
@@ -1,46 +1,46 @@
 import os
-
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 from datetime import datetime
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.all()
-client = discord.Client(command_prefix='!', intents=intents)
+intents = discord.Intents.default()
+intents.message_content = True
 
-@client.event
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
 async def on_ready():
-    print('The Bot is logged in as {0.user}'.format(client))
- 
+    print(f'The Bot is logged in as {bot.user}')
     print('Log:')
-    
-@client.event
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
- 
+
     if message.content.startswith('hi'):
         await message.channel.send('Hello!')
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         print(f'{timestamp} - {message.author}: hi')
-    
-     
-    if message.content.startswith('!info'):
+
+    elif message.content.startswith('!info'):
         await message.channel.send('This is a discord bot, coded in python!\nGithub: https://github.com/arbs09/python-discordbot')
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         print(f'{timestamp} - {message.author}: !info')
 
-    if message.content.startswith('!github'):
+    elif message.content.startswith('!github'):
         await message.channel.send('Github: https://github.com/arbs09/python-discordbot')
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         print(f'{timestamp} - {message.author}: !github')
 
+    await bot.process_commands(message)
 
-@client.event
-async def on_message(message):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(f'{timestamp} - {message.author}: {message.content}')
+@bot.command()
+async def test(ctx, *, arg):
+    await ctx.send(arg)
 
-client.run(TOKEN)
+bot.run(TOKEN)
