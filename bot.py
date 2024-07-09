2ff576235beb2c716e6be3459d03c748fae22df6
arbs09 <info@arbs09.de>
2024-07-09 12:19:53 +0200

    edit

--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
@@ -17,15 +17,8 @@ async def on_ready():
             break
     print('The Bot is logged in as {0.user}'.format(client))
 
-    print(
-        f'The Bot is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+    print('Log:')
     
- 
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -33,9 +26,18 @@ async def on_message(message):
  
     if message.content.startswith('hi'):
         await message.channel.send('Hello!')
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f'{timestamp} - {message.author}: hi')
     
      
     if message.content.startswith('!info'):
         await message.channel.send('This is a discord bot, coded in python!\nGithub: https://github.com/arbs09/python-discordbot')
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f'{timestamp} - {message.author}: !info')
+
+    if message.content.startswith('!github'):
+        await message.channel.send('Github: https://github.com/arbs09/python-discordbot')
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f'{timestamp} - {message.author}: !github')
 
 client.run(TOKEN)
